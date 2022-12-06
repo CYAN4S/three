2915847ba0256af83f2363ef8d8b822440f9e704
CYAN4S <me@cyan4s.com>
2022-12-06 19:21:30 +0900

    Fix mobile motion

--- src/main.ts
+++ src/main.ts
@@ -15,8 +15,8 @@ let y = 0;
 window.addEventListener(
   "devicemotion",
   (ev) => {
-    x = ev.acceleration?.x ? ev.acceleration?.x / 100 : x;
-    y = ev.acceleration?.z ? ev.acceleration?.z / 100 : y;
+    x = ev.acceleration?.x ? ev.acceleration?.x : x;
+    y = ev.acceleration?.y ? ev.acceleration?.y : y;
   },
   true
 );
